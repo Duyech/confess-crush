e530624cfc80f27418568b158346cd19e01ffa96
Duyech <155693881+Duyech@users.noreply.github.com>
2024-01-05 08:41:35 +0700

    Update1 config.js

--- config.js
+++ config.js
@@ -1,15 +1,8 @@
 const CONFIG = {
     titleWeb: "Cái này là dành cho Tèo ngoo",
     introTitle: 'Thanh Trúc ngooooooo',
-    introDesc: `Em hỏi:
-    Tình yêu là gì nhỉ
-    Và mỉm cười thật xinh
-    Nhẹ nhàng tôi thủ thỉ 
-    "Tình yêu là chúng mình".`,
-    btnIntro: 'Anh là người lớn
-        phải nhường em thơ
-        Nhưng tình còn thiếu
-        nên nhờ em thương.',
+    introDesc: `Anh yêu em`,
+    btnIntro: 'Anh có điều muốn nói với em đây',
     title: 'Cảm ơn em đã đồng ý cho anh cơ hội sửa chữa',
     desc: 'Mình cùng xây dựng lại từ đầu em nhé :3 ',
     btnYes: 'Em đồng ý',
